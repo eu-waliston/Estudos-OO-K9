9e7964d0470d4bbc8bda73d72b868c46827b9aea
Waliston Euripedes <goneisgone@hotmail.com>
2022-10-18 06:15:33 -0300

    Threads - FIM

--- Apendice/Threads/TesteTarefa.java
+++ Apendice/Threads/TesteTarefa.java
@@ -15,5 +15,26 @@ public class TesteTarefa {
         threadOI.start();
         thread100.start();
 
+        //Controlando a eecução das tarefas
+        //A clasase thread conta com um set de metodos para controlar a ordem da execução das nossas tarefas
+
+        //Sleep()
+        //Durante a execução de uma tarefa, ao usar o sleep(), a mesma ficara pausada pelo tempo que for passado para o mesmo
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+
+        //Thread.start();
+        //join()
+        //Uma tarefa pode pedir para esperar o termino de outra thread para continuar a execução atraves do método join()
+        //try {
+        //    Thread.join();
+        //} catch (InterruptedException e) {
+        //
+        //}
+
+
     }
 }
